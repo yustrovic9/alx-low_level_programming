61374ac1e37771ca83a7a30e3715a98c74eb3174
yustrovic9 <yustrovic09@gmail.com>
2022-04-08 04:12:33 -0700

    My commit9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, quote, 59);
+	return (1);
+}
