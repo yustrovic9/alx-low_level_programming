af0209cfe222829d463de11e2d4cbf8e790cc87d
yustrovic9 <yustrovic09@gmail.com>
2022-04-28 21:34:31 -0700

    My Commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,20 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
-#include <stdio.h>
-/**
-*void prototypes(void)
-*int prototypes(int)
-*char prototypes(char)
-*/
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-unsigned int _strspn(char *s, char *accept);
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-#endif /* MAIN_H */
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
