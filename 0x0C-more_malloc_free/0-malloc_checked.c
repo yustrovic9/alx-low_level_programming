282207a58444c3222b59f65d0145378b110f58f0
yustrovic9 <yustrovic09@gmail.com>
2022-05-09 13:30:57 -0700

    My commit0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,3 @@
-$ amonkeyprogrammer@ubuntu:~/0x0b. more malloc, free$ cat 0-main.c
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,11 +29,3 @@ int main(void)
     free(d);
     return (0);
 }
-$ amonkeyprogrammer@ubuntu:~/0x0b. more malloc, free$ gcc -Wall -pedantic -Werror -Wextra 0-main.c 0-malloc_checked.c -o a
-$ amonkeyprogrammer@ubuntu:~/0x0b. more malloc, free$ ./a 
-0x1e39010
-0x1e39830
-0x7f31f6c19010
-$ amonkeyprogrammer@ubuntu:~/0x0b. more malloc, free$ echo $?
-98
-$ amonkeyprogrammer@ubuntu:~/0x0b. more malloc, free$
